dd3793486a30c81f75d4918fca0dcbf0b322a3d1
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-05-04 16:28:39 -0300

    feat: add UserRepository

--- auth_security/src/main/java/ms/e_commerce/auth_security/database/repository/UserRepository.java
+++ auth_security/src/main/java/ms/e_commerce/auth_security/database/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package ms.e_commerce.auth_security.database.repository;
 
-public interface UserRepository {
+import ms.e_commerce.auth_security.database.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByUsername(String username);
 }
