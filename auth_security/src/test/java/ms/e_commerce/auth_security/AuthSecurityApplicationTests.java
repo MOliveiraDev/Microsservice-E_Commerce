807ab7a22fa5340ddaa4519310381747aad867ac
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-05-04 00:42:19 -0300

    feat: first commit

---
+++ auth_security/src/test/java/ms/e_commerce/auth_security/AuthSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.e_commerce.auth_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification/src/test/java/ms/e_commerce/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.e_commerce.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
