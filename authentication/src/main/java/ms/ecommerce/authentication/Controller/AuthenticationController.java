636369202a80277e57f627838373e18ad5eb187c
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-05-10 21:28:34 -0300

    feat: add Controller

--- authentication/src/main/java/ms/ecommerce/authentication/Controller/AuthenticationController.java
+++ authentication/src/main/java/ms/ecommerce/authentication/Controller/AuthenticationController.java
@@ -1,4 +1,41 @@
 package ms.ecommerce.authentication.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import ms.ecommerce.authentication.Dtos.AuthenticationRequest;
+import ms.ecommerce.authentication.Dtos.AuthenticationResponse;
+import ms.ecommerce.authentication.Dtos.RegisterRequest;
+import ms.ecommerce.authentication.Service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    //Register
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    //Authenticate
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    //Log out
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
+        authenticationService.logout(request, response);
+        return ResponseEntity.ok().build();
+    }
 }
